57d0f6f3a8c16744c47305084b05b6018cd96b3f
niirmaaltwaatii <twaatiiniirmaal@gmail.com>
2023-07-12 18:06:55 +0545

    init updt

---
+++ prolog/2.pl
@@ -0,0 +1,10 @@
+likes(ram,sita).
+likes(ram,trains).
+likes(suresh,fast_cars).
+likes(Person1,Person2):-
+hobby(Person1,Hobby),
+hobby(Person2,Hobby).
+hobby(ram,trainspotting).
+hobby(saroj,sailing).
+hobby(tina,trainspotting).
+hobby(prakash,sailing).
